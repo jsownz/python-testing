ffacdaa899b151a441457ef4515a1b41d04dcf9e
Jason Sohl <jason@frozensoliddesigns.com>
2015-03-27 16:32:13 -0700

    adding a json example

--- webpy/test.py
+++ webpy/test.py
@@ -1,13 +1,29 @@
 import web
+import json
+# for using templates:
 render = web.template.render('templates/')
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/index.json', 'index_json'
 )
 
 class index:
     def GET(self):
         return render.index()
+
+class index_json:
+    def GET(self):
+      #get the json (could be from a file or just json right here)
+      #jsonData = {'one':1,'two':2}
+      with open('json/index.json') as json_file:
+        jsonData = json.load(json_file)
+
+      #set headers
+      web.header('Content-Type', 'application/json')
+
+      #render json
+      return json.dumps(jsonData)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
